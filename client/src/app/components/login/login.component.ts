6c9d7c6b02fc7e5046bd260c69b7f238860e7c68
Adrian Feria Lopez <adri.gp7@gmail.com>
2018-03-05 19:15:57 +0100

    Add angular 5 client

---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'login',
+  templateUrl: 'login.component.html'
+})
+
+export class LoginComponent implements OnInit{
+    public title:string;
+
+    constructor(){
+      this.title = 'Please, Login'
+    }
+
+    ngOnInit(){
+      console.log('Componente Login cargado')
+    }
+}
